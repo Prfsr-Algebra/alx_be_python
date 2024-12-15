499bf58f8f004acc4d080b72dfbed8880e8ff34d
Prfsr-Algebra <himsluve@gmail.com>
2024-12-15 14:35:53 -0800

    area!

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+# Define the two variables
+length = 10
+width = 5
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Print the result in the specified format
+print(f"The area of the rectangle is: {area}")
