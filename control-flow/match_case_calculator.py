edcea8c27e8d2091b3fd9f091d0198c070f1f2e6
Prfsr-Algebra <himsluve@gmail.com>
2024-12-22 12:31:53 -0800

    another adjustment

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = input("Enter the first number:")
 num2 = input("Enter the second number:")
-operation1 = input("choose the operation (+, -, *, /)")
+operation1 = input("choose the operation (+, -, *, /):")
 match operation1:
     case '+':
         print(f"the result is: {num1 + num2}")
@@ -15,4 +15,4 @@ match operation1:
         if num2 != 0:
             print(f"the result is: {num1 / num2}")
         else:
-            print("invalid division")
+            print("cannot divide by zero.")
