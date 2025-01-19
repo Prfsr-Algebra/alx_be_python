40d1a3c81dbb69be49c6a39c8f1a022e0d599dc3
Prfsr-Algebra <himsluve@gmail.com>
2025-01-19 04:48:51 -0800

    could it be the comment before the conversion factors

--- weather_advice.py
+++
@@ -1,9 +0,0 @@
-weather = input("what's the weather like today? (sunny/rainy/cold):")
-if weather == "sunny":
-    print("wear a t-shirt and sunglasses")
-elif weather == "rainy":
-    print ("Don't forget your umbrella and a raincoat")
-elif weather == "cold":
-    print ("make sure to wear a warm coat and a scarf")
-else:
-    print("sorry, I don't have recommendation for this weather")
